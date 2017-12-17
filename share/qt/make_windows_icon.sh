6e8f2a457ea71dbc3d5db6f553a58653d7d04db8
agsstaff <33987565+agsstaff@users.noreply.github.com>
2017-12-17 12:05:58 -0500

    change icon sizes

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -3,7 +3,7 @@
 ICON_SRC=../../src/qt/res/icons/manna.png
 ICON_DST=../../src/qt/res/icons/manna.ico
 convert ${ICON_SRC} -resize 16x16 manna16.png
-convert ${ICON_SRC} -resize 64x64 manna64.png
-convert ${ICON_SRC} -resize 128x128 manna128.png
+convert ${ICON_SRC} -resize 32x32 manna32.png
+convert ${ICON_SRC} -resize 48x48 manna48.png
 convert manna128.png manna64.png manna16.png ${ICON_DST}
 
